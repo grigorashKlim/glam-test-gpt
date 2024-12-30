f8a58aab1cbd81951814cff95e914ef75357f77c
grigorashKlim <grigorash.klim@gmail.com>
2024-12-30 14:29:52 +0400

    add github pages support

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import MainPage from './components/MainPage';
 import HistoryPage from './components/HistoryPage';
 
 const App = () => {
     return (
         <div className={'main-wrapper'}>
-            <Router>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <Routes>
                     <Route path="/" element={<MainPage />} />
                     <Route path="/user/:userId" element={<HistoryPage />} />
                 </Routes>
-            </Router>
+            </BrowserRouter>
         </div>
     );
 };
